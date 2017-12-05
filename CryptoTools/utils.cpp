8b39983d2a676ce79ad29f7d5a5caa97feca38bb
Anon <kpapageorg24@gmail.com>
2017-12-05 03:38:47 +0200

    Check percentage difference for asks/bids

--- CryptoTools/utils.cpp
+++ CryptoTools/utils.cpp
@@ -24,16 +24,14 @@ int utils::cryptoValue(double cryptoAmount, double cryptoValue) {
 /*
 Percentage difference equals the absolute value of the change in value
 divided by the average of the 2 numbers, all multiplied by 100.
-We then append the percent sign, %, to designate the % difference.
-
-
-int percDif(double x, double y) {
+*/
+double utils::percDif(double x, double y) {
 
 	double avg = (x + y) / 2;
 	double change = x - y;
 	double difference = (change / avg) * 100;
 
-	std::cout << difference << "%" << std::endl;
+	//std::cout << difference << "%" << std::endl;
 
-	return 0;
-}*/
+	return difference;
+}
--- CryptoTools/utils.h
+++ CryptoTools/utils.h
@@ -4,4 +4,5 @@ class utils {
 public:
 	utils();
 	int cryptoValue(double cryptoAmount, double cryptoValue);
+	double percDif(double x, double y);
 };
